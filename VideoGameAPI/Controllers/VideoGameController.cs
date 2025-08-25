460a5c8632bba45beb04ac577aa2d36c51a6e045
bertlomewos <160644577+bertlomewos@users.noreply.github.com>
2025-08-25 14:06:38 +0300

    Add project files.

---
+++ VideoGameAPI/Controllers/VideoGameController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using VideoGameAPI.Data;
+
+namespace VideoGameAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase
+    {
+        private readonly VideoGameDbContext _context = context;
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames()
+        {
+            return Ok(await 
+                _context.videoGames.ToListAsync());
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<VideoGame>> GetVideoGameById(int id)
+        {
+            var game = await _context.videoGames.FindAsync(id);
+            if (game == null)
+                return NotFound();
+            return Ok(game);
+        }
+        [HttpPost]
+        public async Task<ActionResult<VideoGame>> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame == null)
+                return BadRequest();
+            _context.videoGames.Add(newGame);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateVideoGame(int id, VideoGame updatedGame)
+        {
+            var game = await _context.videoGames.FindAsync(id);
+            if (game == null)
+                return NotFound();
+            game.Title = updatedGame.Title;
+            game.Platform = updatedGame.Platform;   
+            game.Developer = updatedGame.Developer;
+            game.Publisher = updatedGame.Publisher;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteVideoGame(int id)
+        {
+            var game = await _context.videoGames.FindAsync(id);
+            if (game == null)
+                return NotFound();
+            _context.videoGames.Remove(game);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ VideoGameAPI/Migrations/20250825093608_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VideoGameAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "videoGames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Platform = table.Column<string>(type: "text", nullable: true),
+                    Developer = table.Column<string>(type: "text", nullable: true),
+                    Publisher = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_videoGames", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "videoGames");
+        }
+    }
+}
